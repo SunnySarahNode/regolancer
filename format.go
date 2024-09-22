08b78bb9bcc73fb352e34a6ef205e7215ac08234
SunnySarahNode <131804784+SunnySarahNode@users.noreply.github.com>
2024-09-22 17:38:20 +0100

    Update format.go

--- format.go
+++ format.go
@@ -23,20 +23,9 @@ func formatAmt(amt int64) string {
 	ms := amt % COIN / 1e6
 	ts := amt % 1e6 / 1e3
 	s := amt % 1e3
-	if btc > 0 {
-		return fmt.Sprintf("%s.%s,%s,%s", infoColorF("%d", btc), infoColorF("%02d", ms),
-			infoColorF("%03d", ts), infoColorF("%03d", s))
-	}
-	if ms > 0 {
-		return fmt.Sprintf("%s,%s,%s", infoColorF("%d", ms), infoColorF("%03d", ts), infoColorF("%03d", s))
-	}
-	if ts > 0 {
-		return fmt.Sprintf("%s,%s", infoColorF("%d", ts), infoColorF("%03d", s))
-	}
-	if s >= 0 {
-		return infoColorF("%d", s)
-	}
-	return errColor("error: ", amt)
+
+	return fmt.Sprintf("%s", hiWhiteColor("%d", btc))
+	
 }
 
 func formatFee(amtMsat int64) string {
