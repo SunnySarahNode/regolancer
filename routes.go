630ad2b84120acabf1d2e87e113b1c5556353b21
SunnySarahNode <131804784+SunnySarahNode@users.noreply.github.com>
2024-09-22 17:44:07 +0100

    Update routes.go

--- routes.go
+++ routes.go
@@ -164,7 +164,7 @@ func (r *regolancer) printRoute(ctx context.Context, route *lnrpc.Route) {
 		}
 		fee := hiWhiteColorF("%-6s", "")
 		if i > 0 {
-			fee = hiWhiteColorF("%-6d", route.Hops[i-1].FeeMsat)
+			fee = hiWhiteColorF("%-3d", route.Hops[i-1].FeeMsat)
 		}
 		fmt.Printf(" %s %s %s | %s ch | %s ₿ | %s \n", cached, faintWhiteColor(fee), infoColor(nodeInfo.Node.Alias),
 			nodeInfo.NumChannels, formatAmt(nodeInfo.TotalCapacity), faintWhiteColor(nodeInfo.Node.PubKey))
